0a105e776a0eea074dbf4f44850c62429e6444c3
soup01 <54523331+soup01@users.noreply.github.com>
2022-07-19 19:32:58 +0300

    Delete config.py

--- config.py
+++
@@ -1,2 +0,0 @@
-token = "OTkzODk2Njc3MDkyMTA2MjQw.G2tYuM.qrwZ5_0iI7AGqbjEmZ2gu58ut5ZaJzKildSGUA"
-mongotok = "mongodb+srv://soup42:bruhDEBIL4256@cluster0.gjfqp.mongodb.net/?retryWrites=true&w=majority"
